e144b13026e8085ae7c168fe3802d2c8118c47b9
ben sadick <bsadick@gmail.com>
2021-07-14 10:42:59 +0900

    added better about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import image from './example text message.PNG'
 import './About.css'
 export default function About() {
   return (
@@ -6,17 +7,18 @@ export default function About() {
       <main>
         <article>
           <header>
-            What's the deal with this site? 👨🏻‍💻
+            How & Why I built this Project 👨🏻‍💻
           </header>
-          <section>
-            I wanted to make a UI for a system I made on AWS. The system I made
-            texts me each day at 10am with the most recent data regarding Corona virus in
-            Seoul and Korea. I was inspired to try out React more formally from
-            a very excellent meetup group called <a className="aLink" target="_blank" rel="noreferrer" href="https://www.meetup.com/New-York-JavaScript-Study-Group/">New York JavaScript Study Group</a>so
-            I put 2 + 2 together and got 22. <br /><br />Okay JavaScript string concatenation
-            jokes aside, I wanted to let other people easily consume data about
-            the status of Covid in Korea. It's my earnest hope that this site helps
-            in some small way.<br /><br />
+          <section className="explanation">
+            <div className="firstPara">
+              This project was built using serverless AWS technologies. It's set up to record the data published on a <a target="_blank" rel="noreferrer" href="http://ncov.mohw.go.kr/en/bdBoardList.do?brdId=16&brdGubun=162&dataGubun=&ncvContSeq=&contSeq=&board_id=">Korean government website</a>and keeping a tab of Corona Virus daily numbers in Seoul and Korea. Every day a CloudWatch set with a cron time expression triggers a lambda function at 10am KST. The lambda function then extracts the data I'm looking for from the Korean governmental website, pushes the data into a DynamoDB table, and sends it to an SNS topic. SNS then fires off which is set up to send a SMS to my cellphone with the most recent data allowing me to keep an eye easily on recent changes in Korea and Seoul.
+            </div>
+            <br />
+            <img src={image} className="exampleTextMessage" alt="an image of recent text messages showing Seoul and Koreas daily covid counts" />
+            <br />
+            After some reflection, I thought that others might be interested in this data for analysis or implenting it in their own way, so a few days back I set up an API Gateway that calls another lambda that talks to the DB and passes back a response to the client.
+            <br /><br />
+            Ultimately, this service was made so other people could easily consume data about the status of Covid in Korea. It's my earnest hope that this site helps others in some small way.<br /><br /><br />
             --Ben🖖🏻
           </section>
         </article>
